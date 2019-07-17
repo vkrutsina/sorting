9ec9c9dfe7371b712c42801f910da81bdf8d1e11
Daniel Chun <dchun016@gmail.com>
2019-07-17 11:02:16 -0500

    working on bubblesort functions

--- bubblesort.js
+++ bubblesort.js
@@ -1,3 +1,24 @@
-function bubbleSort(array) {
+function bubbleSort(arr) {
   /* your code here */
+  if (array.length > 1) {
+    for (let i = 0; i < arr.length; i++) {
+      let currentEle = arr[i];
+
+      toCompare(currentEle, currentEle + 1, arr);
+    }
+  }
+  return arr;
+}
+
+function toCompare(firstEle, secondEle, arr) {
+  if (firstEle > secondEle) {
+    swap(firstEle, secondEle);
+  }
+}
+
+function swap(firstEle, secondEle, arr) {
+  let goingLeft = arr[i - 1];
+  let goingRight = arr[i + 1];
+  goingRight = arr[i];
+  goingLeft = arr[i + 1];
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -2,4 +2,13 @@ describe("Bubble Sort", function() {
   it("handles an empty array", function() {
     expect(bubbleSort([])).toEqual([]);
   });
+  it("sorts in order", function() {
+    expect(bubbleSort([3, 2, 1])).toEqual([1, 2, 3]);
+  });
+  it("elements should be numbers", function() {
+    const arr = ["a", "b"];
+    for (let i = 0; i < arr.length; i++) {
+      expects(typeof arr[i]).toEqual(jasmine.any(Number));
+    }
+  });
 });

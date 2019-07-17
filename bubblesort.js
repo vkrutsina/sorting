5fd58e3e849c12e5f87f9a61ff03bd472c49bf21
Bryan Gottschalk <bryanlgottschalk@gmail.com>
2019-07-17 11:27:03 -0500

    started splicing

--- bubblesort.js
+++ bubblesort.js
@@ -1,24 +1,36 @@
 function bubbleSort(arr) {
   /* your code here */
-  if (array.length > 1) {
+  if (arr.length > 1) {
     for (let i = 0; i < arr.length; i++) {
       let currentEle = arr[i];
-
-      toCompare(currentEle, currentEle + 1, arr);
+      if (toCompare(currentEle, currentEle + 1)) {
+        // returns true if first ele is larger than second, else false
+        swap(arr, i);
+      }
     }
   }
   return arr;
 }
 
-function toCompare(firstEle, secondEle, arr) {
+function toCompare(firstEle, secondEle) {
   if (firstEle > secondEle) {
-    swap(firstEle, secondEle);
+    return true;
+  } else {
+    return false;
   }
 }
 
-function swap(firstEle, secondEle, arr) {
-  let goingLeft = arr[i - 1];
-  let goingRight = arr[i + 1];
-  goingRight = arr[i];
-  goingLeft = arr[i + 1];
+function swap(arr, curIdx) {
+  // first ele is larger than second
+  // store what we're replacing
+  // replace the right with the left
+  // place what we stored into the left
+
+  let firstEle = arr[curIdx + 1];
+  let secondEle = arr[i - 1];
+
+  return arr.splice(secondEle, 1, firstEle);
+
+  // goingRight = arr[i];
+  // goingLeft = arr[i + 1];
 }
